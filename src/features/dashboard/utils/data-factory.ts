b2a5ca5fe027f978f0af6cebc63d058f516b04db
Mudoker <huuquoc7603@gmail.com>
2024-11-04 18:09:35 +0700

    feat: Integrate usage data handling in CurrentPlanLimitContainer and enhance DataFactory for dynamic data generation

--- src/features/dashboard/utils/data-factory.ts
+++ src/features/dashboard/utils/data-factory.ts
@@ -1,5 +1,14 @@
 import { ViewMode } from "@/features/dev-dock/data/type";
 import { generateRandomRecentActivity } from "./calculator";
+import { faker } from "@faker-js/faker";
+
+export type UsageDataType = {
+    projects: number;
+    issues: number;
+    storage: number;
+    fileUpload: number;
+    tokensUsed: number;
+}
 
 export class DataFactory {
     public static getRecentActivityData(dataViewMode: ViewMode): any {
@@ -12,5 +21,35 @@ export class DataFactory {
 
         // Should be replaced with real data
         return generateRandomRecentActivity(12);
+    }
+
+    public static getUsageData(dataViewMode: ViewMode): any {
+        if (dataViewMode === ViewMode.NO_DATA) {
+            return {
+                projects: 0,
+                issues: 0,
+                storage: 0,
+                fileUpload: 0,
+                tokensUsed: 0,
+            };
+        }
+        if (dataViewMode === ViewMode.FAKE_DATA) {
+            return {
+                projects: faker.number.int({ min: 0, max: 10 }),
+                issues: faker.number.int({ min: 0, max: 100 }),
+                storage: faker.number.int({ min: 0, max: 5 }),
+                fileUpload: faker.number.int({ min: 0, max: 1 }),
+                tokensUsed: faker.number.int({ min: 0, max: 1 }),
+            };
+        }
+
+        // Should be replaced with real data
+        return {
+            projects: faker.number.int({ min: 0, max: 10 }),
+            issues: faker.number.int({ min: 0, max: 100 }),
+            storage: faker.number.int({ min: 0, max: 5 }),
+            fileUpload: faker.number.int({ min: 0, max: 1 }),
+            tokensUsed: faker.number.int({ min: 0, max: 1 }),
+        };
     }
 }
